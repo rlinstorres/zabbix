21a78c1e2590e401d0ac4b71cb5f9b65fd4ff192
Jailson <jailson@vagas.com.br>
2018-01-21 11:38:30 -0200

    Add generated delivery build cookbook

---
+++ .delivery/build_cookbook/metadata.rb
@@ -0,0 +1,8 @@
+name 'build_cookbook'
+maintainer 'Jailson Silva'
+maintainer_email 'jailson.silva@outlook.com.br'
+license 'apachev2'
+version '0.1.0'
+chef_version '>= 12.1' if respond_to?(:chef_version)
+
+depends 'delivery-truck'
